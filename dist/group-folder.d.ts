9cf6d88496308ad62b973bdc2a2c2a35e46defa3
xluwang <xluwang@qq.com>
2026-04-25 23:58:30 +0800

    Delete dist directory

--- dist/group-folder.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function isValidGroupFolder(folder: string): boolean;
-export declare function assertValidGroupFolder(folder: string): void;
-export declare function resolveGroupFolderPath(folder: string): string;
-export declare function resolveGroupIpcPath(folder: string): string;
-//# sourceMappingURL=group-folder.d.ts.map
